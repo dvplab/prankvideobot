b08804164e36a98de5c5d7050ab545adadae2a3f
dvplab <opset9922@gmail.com>
2024-11-17 11:27:46 +0300

    Исправление __dirname

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bot from '../bot/bot.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
